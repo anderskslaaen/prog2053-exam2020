5681ab740c009e87ccf52c4319642dfa8c637fae
anderskslaaen <anderskslaaen@gmail.com>
2020-12-17 11:54:00 +0100

    Update edit-user.js
    drit

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -7,7 +7,30 @@ class EditUser extends LitElement {
     };
   }
 
-  // din kode her
+  render() {
+    return html`
+        <div>
+          <p>Username: ${this.user.uname}</p>
+          <p>First Name: ${this.user.firstName}</p>
+          <p>Last Name: ${this.user.lastName}</p>
+        </div>
+        <form action="../api/updateUser.php">
+          <div>
+            <label>New username</label>
+            <input id="newUName"> <br><br>
+            <label>New first name</label>
+            <input id="newFName"> <br><br>
+            <label>New Last name</label>
+            <input id="newLName"> <br><br>
+            <label>Password: </label>
+            <input id="pwd" type="password" required> <br><br>
+          </div>
+          <button >Submit</button>
+        </form>
+    `;
+  }
 
+  
+  
 }
 customElements.define('edit-user', EditUser);
